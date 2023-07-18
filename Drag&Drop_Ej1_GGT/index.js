ed1333b7d88cd0651db36dc08ff86f7d7528c27c
Guada <125298307+GuadaGT@users.noreply.github.com>
2023-07-18 16:25:23 +0200

    Add files via upload

---
+++ Drag&Drop_Ej1_GGT/index.js
@@ -0,0 +1,42 @@
+const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".seccion")
+const papelera = document.getElementById("papelera")
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", event => {
+        console.log("Estoy arrastrando el párrafo: " + parrafo.innerText)
+        parrafo.classList.add("dragging")
+        event.dataTransfer.setData("id", parrafo.id)
+        const elemento_fantasma = document.querySelector(".imagen-fantasma")
+        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0)
+    })
+
+    parrafo.addEventListener("dragend", () => {
+        parrafo.classList.remove("dragging")
+    })
+})
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", event => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "copy"
+    })
+
+    seccion.addEventListener("drop", event => {
+        console.log("Drop")
+        const id_parrafo = event.dataTransfer.getData("id")
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
+    })
+})
+
+papelera.addEventListener("dragover", event => {
+    event.preventDefault()
+})
+
+papelera.addEventListener("drop", event => {
+    console.log("Eliminado")
+    const id_parrafo = event.dataTransfer.getData("id")
+    const parrafo = document.getElementById(id_parrafo)
+    parrafo.remove()
+})
